f78cbfb89dd748722f43b8cbc14d3b92467877ad
Shoxrux <ganiorzikulov@gmail.com>
2024-01-31 18:33:20 +0500

    f

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import Phone from "./Phone"
+
+let phones = [
+    {
+        id: 1,
+        name: 'Redmi 12',
+        img: 'https://vega.am/image/cache/catalog/1HRACH/2020/2021/2023/Ogostos/Smartfon/XIAOMI%20REDMI%2012%208GB%20256GB%20Black%20(1)-2000x1500.jpg',
+        month: '1 920сум/мес',
+        promotion: '16 000сум'
+    }
+]
+
+
+function App() {
+    return (
+        <div className="container">
+            <div className="w=[90%] flex flex-wrap gap-20 mt-10 justify-center ">
+                {
+                    phones.map(item => (
+                        <Phone key={item.id} {...item} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default App
