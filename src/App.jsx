ad8bc14c6a04d8320497827511b20b0de72be62d
Shoxrux <ganiorzikulov@gmail.com>
2024-01-31 18:45:40 +0500

    rename phone

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Phone from "./Phone"
 let phones = [
     {
         id: 1,
-        name: 'Redmi 12',
+        name: 'iPhone',
         img: 'https://vega.am/image/cache/catalog/1HRACH/2020/2021/2023/Ogostos/Smartfon/XIAOMI%20REDMI%2012%208GB%20256GB%20Black%20(1)-2000x1500.jpg',
         month: '1 920сум/мес',
         promotion: '16 000сум'
